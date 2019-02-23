846e610ec4ed2e2066f98446833afb1e642d9b68
Luis Petrella <aax_00ap@hotmail.com>
2019-02-23 17:35:20 -0400

    Hey nosiento amiko
    Me estoy muriendo

---
+++ react-app/src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Segment, Image, Form, Container, Message, Divider, Transition } from 'semantic-ui-react'
+import button from './Button'
+
+export default class Profile extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	
+}
--- react-app/src/views/Dashboard.js
+++ react-app/src/views/Dashboard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Menu, Label, Input, Icon, Container, Grid } from 'semantic-ui-react'
+import Home from '../component/Home'
+import Inbox from '../component/Inbox'
+import Profile from '../component/Profile'
+import Search from '../component/Search'
+import Settings from '../component/Settings'
 
 export default class Dashboard extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {activeItem : 'Home'}
 	}
-
-	state = {activeItem : 'Home'}
 
 	handleItemClick = (evt, {name}) => {
 		this.setState({activeItem : name});
@@ -34,7 +38,13 @@ export default class Dashboard extends React.Component {
 					</Menu>
 				</Grid.Column>
 				<Grid.Column>
-
+					<Container>
+						<Home/>
+						<Inbox/>
+						<Profile/>
+						<Search/>
+						<Settings/>
+					</Container>
 				</Grid.Column>
 			</Grid>
 			);
