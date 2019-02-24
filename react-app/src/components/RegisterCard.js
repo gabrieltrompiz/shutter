9099f9a8cf1cc48ccb3915126318ea4b0eaa12db
Gabriel Trompiz <gab.tc@icloud.com>
2019-02-23 20:46:40 -0400

    register bug fixing

--- react-app/src/components/RegisterCard.js
+++ react-app/src/components/RegisterCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Segment, Form, Container, Transition, Checkbox, Step, Input, Icon, Header } from 'semantic-ui-react'
+import { Segment, Form, Container, Transition, Step, Input } from 'semantic-ui-react'
+import { DateInput } from 'semantic-ui-calendar-react'
 import Button from './Button'
 
 export default class RegisterCard extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { step: 0, pwdVisible: false, confVisible: false }
+        this.state = { step: 0, pwdVisible: false, confVisible: false, firstname: '', lastname: '', birthday: '',
+        gender: '', username: '', email: '', password: '', passwordConf: '' }
+        this.handleInput = this.handleInput.bind(this)
+        this.validator = require('email-validator')
     }
 
     handleClickPwd = () => {
@@ -16,11 +20,17 @@ export default class RegisterCard extends React.Component {
         this.setState({ confVisible: !this.state.confVisible })
     }
 
+    handleInput = (event, {name, value}) => {
+        this.setState({ [name]: value })
+    }
+
     render() {
         const iconPwd = this.state.pwdVisible ? 'unhide' : 'hide'
         const iconConf = this.state.confVisible ? 'unhide' : 'hide'
         const typePwd = this.state.pwdVisible ? 'text' : 'password'
         const typeConf = this.state.confVisible ? 'text' : 'password'
+        const today = new Date()
+        const maxDate = today.getDate() + '-' + (parseInt(today.getMonth(), 10) + 1) + '-' + (parseInt(today.getFullYear(), 10) - 12) // Get current date in format dd-mm-yyyy - 12 years
         return(
             <Transition visible={this.props.visible} transitionOnMount unmountOnHide duration={350}>
                 <Container>
@@ -35,32 +45,32 @@ export default class RegisterCard extends React.Component {
                             active={this.state.step === 2} disabled={this.state.step !== 2} completed={this.state.step > 2} />      
                         </Step.Group>
                         {this.state.step === 0 && 
-                        <Form style={{ marginLeft: 10, marginRight: 10, width: '97%' }} size='large'>
+                        <Form style={{ marginLeft: 10, marginRight: 10, width: '97%' }} size='large' autoComplete='off'>
                             <Form.Group widths='equal'> 
-                                <Form.Field error={this.state.errorUsername} placeholder="First name" onChange={this.handleChange} 
-                                control={Input} label="First name"/>
-                                <Form.Field error={this.state.errorUsername} placeholder="Last name" onChange={this.handleChange} 
-                                control={Input} label="Last name"/>
+                                <Form.Field placeholder="First name" onChange={this.handleInput} 
+                                control={Input} label="First name" name='firstname' value={this.state.firstname}/>
+                                <Form.Field placeholder="Last name" onChange={this.handleInput} 
+                                control={Input} label="Last name" name='lastname' value={this.state.lastname}/>
                             </Form.Group>
                             <Form.Group widths='equal'>
-                                <Form.Field error={this.state.errorUsername} placeholder="Birthday" onChange={this.handleChange} 
-                                control={Input} label="Birthday" type="date"/>
-                                <Form.Select label='Gender' options={options} placeholder='Gender' />
+                                <Form.Field label="Birthday" control={DateInput} value={this.state.birthday} iconPosition='left' 
+                                onChange={this.handleInput} name='birthday' closable placeholder='Click to select a date' maxDate={maxDate} initialDate={maxDate}/>
+                                <Form.Select label='Gender' options={options} placeholder='Gender' name='gender' onChange={this.handleInput} value={this.state.gender}/>
                             </Form.Group>
                             {/* <Form.Field control={Checkbox} label='I agree to the Terms and Conditions' /> */}
                         </Form>}
                         {this.state.step === 1 &&
                         <Form style={{ marginLeft: 10, marginRight: 10, width: '97%' }} size='large'>
                             <Form.Group widths='equal'>
-                                <Form.Field error={this.state.errorUsername} placeholder="Username" onChange={this.handleChange} 
-                                control={Input} label="Username" icon='at' iconPosition="left"/>
-                                <Form.Field error={this.state.errorUsername} placeholder="Email" onChange={this.handleChange} 
-                                control={Input} label="Email" type='email'/>
+                                <Form.Field placeholder="Username" onChange={this.handleInput} action={{ icon: 'check' }}
+                                control={Input} label="Username" icon='at' iconPosition="left" name='username'/>
+                                <Form.Field placeholder="Email" onChange={this.handleInput} 
+                                control={Input} label="Email" type='email' name='email'/>
                             </Form.Group>
                             <Form.Group widths='equal'>
-                                <Form.Field error={this.state.errorUsername} placeholder="Password" onChange={this.handleChange} 
-                                control={Input} label="Password" type={typePwd} action={{ icon: iconPwd, onClick: this.handleClickPwd }}/>
-                                <Form.Field error={this.state.errorUsername} placeholder="Confirm password" onChange={this.handleChange} 
+                                <Form.Field placeholder="Password" onChange={this.handleInput} 
+                                control={Input} label="Password" type={typePwd} action={{ icon: iconPwd, onClick: this.handleClickPwd }} name='password'/>
+                                <Form.Field placeholder="Confirm password" onChange={this.handleInput} name='passwordConf'
                                 control={Input} label="Confirm password" type={typeConf} action={{ icon: iconConf, onClick: this.handleClickConf }}/>
                             </Form.Group>
                         </Form>}
