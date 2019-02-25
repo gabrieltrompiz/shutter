24ab6532e9b1daaa5629aac3a1355514721da259
Luis Petrella <aax_00ap@hotmail.com>
2019-02-25 17:57:41 -0400

    auxilio
    si esta verga se rompe me voy a suicidar

--- react-app/src/components/Profile.js
+++ react-app/src/components/Profile.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { Segment, Image, Form, Container, Message, Divider, Transition } from 'semantic-ui-react'
+import { Segment, Image, Form, Container, Message, Divider, Transition, Grid, Header } from 'semantic-ui-react'
 import button from './Button'
 
 export default class Profile extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {}
 	}
-
 	
+	render() {
+		return(
+			<Container fluid>
+				<Grid>
+					<Grid.Column>
+						<div style={{marginTop: '3vh'}}>
+							<Image
+								src={require('../assets/pandagram2.png')}
+								style={{ width: '200px', height: '200px', textAlign: 'center' }}
+							/>
+							<Header as='h3' style={{align: 'center'}}>Nickname</Header>
+						</div>
+
+					</Grid.Column>
+				</Grid>
+			</Container>
+			);
+	}
+
 }
--- react-app/src/views/Dashboard.js
+++ react-app/src/views/Dashboard.js
@@ -14,7 +14,8 @@ export default class Dashboard extends React.Component {
 
 	handleItemClick = (evt, {name}) => {
 		this.setState({ activeItem : name });
-	}
+		console.log(name);
+	};
 
 	logout = async () => {
 		await fetch('http://localhost:8080/logout', { credentials: 'include' })
@@ -24,55 +25,60 @@ export default class Dashboard extends React.Component {
 	render() {
 		return (
 			<Container fluid style={{ height: 'inherit', backgroundColor: '#FAFAFC' }}>
-				<div style={{ height: '90vh', paddingTop: '5vh', float: 'left' }}>
-					<Menu vertical style={{ height: 'inherit' }} pointing>
-						<Menu.Item header style={{ textAlign: 'center' }}>
-							<Image 
-								as="img" 
-								src={require('../assets/pandagram.png')}
-								style={{ width: 55, height: 55, borderRadius: 12, margin: 'auto', marginTop: 10 }}
-							/>
-							<p style={{ fontFamily: 'Billabong', fontSize: 40, fontWeight: '400' }}>Pandagram</p>
-						</Menu.Item>
-						<Menu.Item active={this.state.activeItem === 'Home'} onClick={this.handleItemClick} name='Home'>
-							<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
-								<Icon name='home' style={{ float: 'left', fontSize: 16 }}/>
-								Home
-							</Header>
-						</Menu.Item>
-						<Menu.Item active={this.state.activeItem === 'Profile'} onClick={this.handleItemClick} name='Profile'>
-							<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
-								<Icon name='user' style={{ float: 'left', fontSize: 16 }}/>
-								Profile
-							</Header>
-						</Menu.Item>
-						<Menu.Item active={this.state.activeItem === 'Inbox'} onClick={this.handleItemClick} name='Inbox'>
-							<Label color='teal'>1</Label>
-							<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
-								<Icon name='inbox' style={{ float: 'left', fontSize: 16 }}/>
-								Inbox
-							</Header>
-						</Menu.Item>
-						<Menu.Item active={this.state.activeItem === 'Search'} onClick={this.handleItemClick} name='Search'>
-							<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
-								<Icon name='search' style={{ float: 'left', fontSize: 16 }}/>
-								Search
-							</Header>
-						</Menu.Item>
-						<Menu.Item active={this.state.activeItem === 'Settings'} onClick={this.handleItemClick} name='Settings'>
-							<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
-								<Icon name='cog' style={{ float: 'left', fontSize: 16 }}/>
-								Settings
-							</Header>
-						</Menu.Item>
-						<Menu.Item active={this.state.activeItem === 'LogOut'} onClick={this.logout}>
-							<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
-								<Icon name='sign out' style={{ float: 'left', fontSize: 16 }}/>	
-								Log out
-							</Header>
-						</Menu.Item>
-					</Menu>
-				</div>
+				<Grid>
+					<Grid.Column width={4} style={{ height: '90vh', paddingTop: '5vh', float: 'left' }}>
+						<Menu vertical style={{ height: 'inherit' }} pointing>
+							<Menu.Item header style={{ textAlign: 'center' }}>
+								<Image
+									as="img"
+									src={require('../assets/pandagram.png')}
+									style={{ width: 55, height: 55, borderRadius: 12, margin: 'auto', marginTop: 10 }}
+								/>
+								<p style={{ fontFamily: 'Billabong', fontSize: 40, fontWeight: '400' }}>Pandagram</p>
+							</Menu.Item>
+							<Menu.Item active={this.state.activeItem === 'Home'} onClick={this.handleItemClick} name='Home'>
+								<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
+									<Icon name='home' style={{ float: 'left', fontSize: 16 }}/>
+									Home
+								</Header>
+							</Menu.Item>
+							<Menu.Item active={this.state.activeItem === 'Profile'} onClick={this.handleItemClick} name='Profile'>
+								<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
+									<Icon name='user' style={{ float: 'left', fontSize: 16 }}/>
+									Profile
+								</Header>
+							</Menu.Item>
+							<Menu.Item active={this.state.activeItem === 'Inbox'} onClick={this.handleItemClick} name='Inbox'>
+								<Label color='teal'>1</Label>
+								<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
+									<Icon name='inbox' style={{ float: 'left', fontSize: 16 }}/>
+									Inbox
+								</Header>
+							</Menu.Item>
+							<Menu.Item active={this.state.activeItem === 'Search'} onClick={this.handleItemClick} name='Search'>
+								<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
+									<Icon name='search' style={{ float: 'left', fontSize: 16 }}/>
+									Search
+								</Header>
+							</Menu.Item>
+							<Menu.Item active={this.state.activeItem === 'Settings'} onClick={this.handleItemClick} name='Settings'>
+								<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
+									<Icon name='cog' style={{ float: 'left', fontSize: 16 }}/>
+									Settings
+								</Header>
+							</Menu.Item>
+							<Menu.Item active={this.state.activeItem === 'LogOut'} onClick={this.handleItemClick} name='LogOut'>
+								<Header as='h5' style={{ paddingLeft: 10, marginTop: 0.5 }}>
+									<Icon name='sign out' style={{ float: 'left', fontSize: 16 }}/>
+									Log out
+								</Header>
+							</Menu.Item>
+						</Menu>
+					</Grid.Column>
+					<Grid.Column width={12}>
+						<Profile/>
+					</Grid.Column>
+				</Grid>
 			</Container>
 			
 			// <Container>
